505942b0157b6a77b668ab298cfaf986134c2524
Hyunkoo <dknohyou@gmail.com>
2017-07-04 19:29:36 +0000

    Finished to Exercise 10

--- program.js
+++ program.js
@@ -119,6 +119,7 @@ function callback (response) {
 //setEncoding()
 */
 
+/*
 // Lession 8: HTTP Collect
 // Goal: Collect all data from server and write two lines to console
 // First line: integer - number of characters from server
@@ -126,16 +127,124 @@ function callback (response) {
 
 var http = require('http')
 var website = process.argv[2]
+var bl = require('bl')
 var content  = []
 http.get(website, function(response){
-    content = response.on("data",function(data){
+    //content = response.on("data",function(data){
         //console.log(data.toString())
         //console.log(typeof(data.toString()))
-        content.push(data.toString())
+        //content.push(data.toString())
         //console.log(content)
         //console.log(data.length)
-    })
-    console.log(content)
+    //})
+    //console.log(content)
     //console.log(content.length)
+    response.pipe(bl(function(err,data){
+        console.log(data.toString().length)
+        console.log(data.toString())
+    }))
+    
 } )
+*/
 
+/*
+// Lession 9: Juggling Async
+// Goal: Output the string from the three URLS
+
+var http = require('http')
+var bl = require('bl')
+var count = 0
+var results = []
+
+function printResult(){
+    for (var i=0; i<3; i++)
+    //console.log(i)
+    console.log(results[i])
+}
+
+function httpGet(index){
+http.get(process.argv[2 + index], function(response){
+    //console.log(response)
+    //console.log(process.argv[2 + index])
+    //console.log(index)
+    response.pipe(bl(function(err,data){
+        //console.log(data.toString())
+        results[index] = data.toString()
+        count++
+        
+         if (count == 3)
+        printResult()
+        
+    }))
+   
+})
+}
+
+for (var i=0; i<3; i++){
+    httpGet(i)
+}
+*/
+
+/*
+// Exercise Time Server 10 
+// Goal: 1) Server listen to TCP connections
+// 2) write out current date
+// 3) After sending string close connection 
+
+var net = require('net')
+//console.log(process.argv)
+var serverHost = process.argv[2]
+
+var server = net.createServer(function listener(socket){
+    // Socket handling logic
+    var date = new Date().toISOString().replace(/T/,' ').replace(/\..+/,'').substr(0,16) + '\n'
+    //console.log(date)
+    socket.write(date)
+    //printDate = date.getFullYear().toString()+date.getMonth().toString+date.getDay().toString
+    //console.log(socket)
+    //date.getFullYear()
+    //console.log(printDate)
+    socket.end()
+})
+
+server.listen(serverHost)
+*/
+
+// Exercise 11: HTTP File Server
+// Goal: Write HTTP Server serves same text file for each request it receives
+// Server listen on port - !st Argument
+// Location of the file to serve - 2nd command line argument
+// fs.createReadStream() stream file contents to response
+
+var http = require('http')
+var bl = require('bl')
+var fs = require('fs')
+var website = process.argv[2]
+var content = process.argv[3]
+console.log(website)
+console.log(content)
+
+    
+
+var server = http.createServer(function callback (request, response) {
+    // request: fetch properties such as header 
+    //http.get(website,function (response){
+    //console.log(request)
+    //console.log(response)
+    //    response.pipe(bl(function(err,data){
+    //    console.log(data.toString())
+        //results[index] = data.toString()
+        //count++
+    //    }))
+    //})
+    //console.log(content)
+    // response: sending data to client both header and body
+    //.setHeader('Hi')
+    readStream = fs.createReadStream(content).pipe(response)
+    console.log(readStream)
+    // Both request and response are Node streams
+    //console.log(response)
+    //response.end()
+})
+
+server.listen(website)
